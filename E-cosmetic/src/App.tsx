e01f312acb9ba3a571077030392c47185586ca3f
PhanDucTho <tho.phan@devplus.edu.vn>
2023-08-23 15:54:44 +0700

    test

--- E-cosmetic/src/App.tsx
+++ E-cosmetic/src/App.tsx
@@ -26,7 +26,6 @@ function App() {
   return (
     <>
     <Header/>
-
       <Routes>
 
 <Route path="/" element={<Home />} />
--- E-cosmetic/src/component/atoms/breadcrumb/breadcrumb.tsx
+++ E-cosmetic/src/component/atoms/breadcrumb/breadcrumb.tsx
@@ -7,13 +7,18 @@ export default function BreadcrumbComponent() {
   return (
     <Breadcrumb style={{ marginTop: "3%",marginLeft:"7.5%" }}>
       <Breadcrumb.Item>
-        <Link to="/HomePage">
+        <Link to="/">
           <b style={{ color: "#8CAE71" }}>HomePage</b>
         </Link>
       </Breadcrumb.Item>
       <Breadcrumb.Item>
         <Link to={`/products/${product_name}/${id}`}>
           <b style={{ color: "#8CAE71" }}>{product_name}</b>
+        </Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Item>
+        <Link to={`/products`}>
+          <b style={{ color: "#8CAE71" }}>Products</b>
         </Link>
       </Breadcrumb.Item>
     </Breadcrumb>
