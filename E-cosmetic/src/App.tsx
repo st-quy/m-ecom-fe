f4d718bbc507bba84c41a2f35722f6e08b38d1bf
PhanDucTho <tho.phan@devplus.edu.vn>
2023-08-24 10:10:35 +0700

    feature/homepage

--- E-cosmetic/src/App.tsx
+++ E-cosmetic/src/App.tsx
@@ -1,57 +1,48 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './pages/HomePage/header/header';
-import Home from './pages/HomePage/home/home';
-import Footer from './pages/HomePage/footer/footer';
-import Detail from './pages/HomePage/detail/detail';
-import Register from './pages/HomePage/register/register';
-import Login from './pages/HomePage/login/login';
-import BreadcrumbComponent from './component/atoms/breadcrumb/breadcrumb';
-import AddProductForm from './pages/AdminPage/product/addproduct';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './pages/HomePage/header/header'
+import Home from './pages/HomePage/home/home'
+import Footer from './pages/HomePage/footer/footer'
+import Detail from './pages/HomePage/detail/detail'
+import Register from './pages/HomePage/register/register'
+import Login from './pages/HomePage/login/login'
+import BreadcrumbComponent from './component/atoms/breadcrumb/breadcrumb'
+import Dashboard from './component/atoms/dashboard/dashboard'
 
 function App() {
   return (
-
     <Router>
       <Routes>
-
-
-      <Route path="/sign-in" element={<Login />} />
-  <Route path="/sign-up" element={<Register />} /> 
-  <Route path="*" element={<PageUser />} />
-   <Route path="/products/:product_name/:id" element={<Pagedetail />}/>
+        <Route path='*' element={<Dashboard />} />
+        <Route path='/sign-in' element={<Login />} />
+        <Route path='/sign-up' element={<Register />} />
+        <Route path='*' element={<PageUser />} />
+        <Route path='/products/:product_name/:id' element={<Pagedetail />} />
       </Routes>
     </Router>
   )
 }
- function PageUser() {
+function PageUser() {
   return (
     <>
-    <Header/>
+      <Header />
 
       <Routes>
-
-<Route path="/" element={<Home />} />
-
-</Routes>
-<Footer/>
-
-</>
-  );
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 function Pagedetail() {
   return (
     <>
-    <Header/>
-    <BreadcrumbComponent></BreadcrumbComponent>
+      <Header />
+      <BreadcrumbComponent></BreadcrumbComponent>
       <Routes>
-
-<Route path="/" element={<Detail />} />
-
-</Routes>
-<Footer/>
-
-</>
-  );
+        <Route path='/' element={<Detail />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 export default App
